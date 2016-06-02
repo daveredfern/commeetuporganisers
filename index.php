e94fc593b5128489bc295ffd6b25e96292d60b72
Dave Redfern <dave@daveredfern.com>
2016-06-02 21:16:30 +0100

    Adding link to Staffs Web Meetup

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     </header>
     <div class="u-container u-section">
         <p>Hello fellow meetup organisers,</p>
-        <p>Our names are <a href="https://twitter.com/daveredfern">Dave Redfern</a>, <a href="https://twitter.com/mustardbees">Phil Wylie</a> and <a href="https://twitter.com/natasha_allsopp">Natasha Allsopp</a>. We founded Staffs Web Meetup about 18 months ago. During that time we’ve met some great people, learned a lot and seen some terrific speakers. Organising a meetup is by no means a small feat so that got us thinking. How can we make our lives easier?</p>
+        <p>Our names are <a href="https://twitter.com/daveredfern">Dave Redfern</a>, <a href="https://twitter.com/mustardbees">Phil Wylie</a> and <a href="https://twitter.com/natasha_allsopp">Natasha Allsopp</a>. We founded <a href="https://staffswebmeetup.co.uk/">Staffs Web Meetup</a> about 18 months ago. During that time we’ve met some great people, learned a lot and seen some terrific speakers. Organising a meetup is by no means a small feat so that got us thinking. How can we make our lives easier?</p>
         <p>A few months ago we thought it might be a good idea for meetup organisers to get together every so often &mdash; a meetup of meetup organisers. Our plan is simple. To arrange a get together with a bunch of great people. Nothing fancy (organising one meetup is plenty), no talks or schedules, just a nice evening out.</p>
         <p>The aim for the evening is to share ideas, problems, successes and contacts. Obviously a drink (or two) and some tasty grub wouldn’t go amiss either.</p>
         <p>We think Birmingham is a good location. It’s central, good access to public transport and most importantly, lots of good places to eat! On that note, if you can recommend anywhere for food, let us know!</p>
@@ -29,7 +29,7 @@
 
     <div class="u-well u-container u-container--lg u-section">
         <form action="https://tinyletter.com/meetuporganisers" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/meetuporganisers', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
-            <h2>Interested? Sign up to mailing list</h2>
+            <h2>Interested? Sign up for more details</h2>
             <label for="tlemail">Enter your email address</label>
             <div class="u-flag">
                 <input type="text" name="email" id="tlemail" placeholder="Your email address" />
