fa008c251d3c8706990d76af508131706d2615f8
Dave Redfern <dave@daveredfern.com>
2016-06-02 20:53:41 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html lang="en-GB" prefix="og: http://ogp.me/ns#" class="single single-post postid-593 single-format-standard">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <style type="text/css" media="all">
+        <?php echo file_get_contents('build/css/styles.css'); ?>
+    </style>
+    <title>Meetup of Meetup.com meetup organisers</title>
+</head>
+
+<body>
+    <header class="u-container u-section">
+        <h1>A meetup of meetup organisers</h1>
+    </header>
+    <div class="u-container u-section">
+        <p>Hello fellow meetup organisers,</p>
+        <p>Our names are <a href="https://twitter.com/daveredfern">Dave Redfern</a>, <a href="https://twitter.com/mustardbees">Phil Wylie</a> and <a href="https://twitter.com/natasha_allsopp">Natasha Allsopp</a>. We founded Staffs Web Meetup about 18 months ago. During that time we’ve met some great people, learned a lot and seen some terrific speakers. Organising a meetup is by no means a small feat so that got us thinking. How can we make our lives easier?</p>
+        <p>A few months ago we thought it might be a good idea for meetup organisers to get together every so often &mdash; a meetup of meetup organisers. Our plan is simple. To arrange a get together with a bunch of great people. Nothing fancy (organising one meetup is plenty), no talks or schedules, just a nice evening out.</p>
+        <p>The aim for the evening is to share ideas, problems, successes and contacts. Obviously a drink (or two) and some tasty grub wouldn’t go amiss either.</p>
+        <p>We think Birmingham is a good location. It’s central, good access to public transport and most importantly, lots of good places to eat! On that note, if you can recommend anywhere for food, let us know!</p>
+        <p>We haven’t set a date yet. At this stage we’re just gauging interest. We’re thinking of creating a questionnaire with a few days and dates, the most popular wins.</p>
+        <p>For now, we’ve created a little mailing list and we’ll be in touch soon. If all goes well, we can make this more regular &mdash; something to discuss on the evening.</p>
+        <p>So if you’re interested, sign up below and we’ll be in touch. If you know any other web meetup organisers feel free to pass this link on &mdash; the more the merrier!</p>
+        <p>Thanks,</p>
+        <p>Dave, Phil &amp; Natasha.</p>
+        <p>P.s. Is there an award for the most ridiculous domain name?</p>
+    </div>
+
+    <div class="u-container u-section">
+        <hr />
+    </div>
+
+    <div class="u-container u-section">
+        <h2>Interested? Sign up to mailing list</h2>
+        <form action="https://tinyletter.com/meetuporganisers" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/meetuporganisers', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+            <label for="tlemail">Enter your email address</label>
+            <div class="u-flag">
+                <input type="text" name="email" id="tlemail" />
+                <input type="submit" value="Subscribe" class="button" />
+            </div>
+            <input type="hidden" value="1" name="embed"/>
+            <p class="micro">Of course, we won't share or spam your email</p>
+        </form>
+    </div>
+
+</body>
+
+</html>
